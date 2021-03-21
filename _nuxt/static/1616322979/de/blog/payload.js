36222621bdaa88fde7abc44e3d3d40d75a5cf79e
Jan Müller <janmueller3698@gmail.com>
2021-03-21 10:36:49 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@2877d35d8878d6ad27ebddbd31466fdd3e70f0ee 🚀

---
+++ _nuxt/static/1616322979/de/blog/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/de/blog", (function(a){return {data:[{posts:[{title:"Nuxt, Vue and TypeScript",path:"\u002Fde\u002Fblog\u002Fnuxt-vue-and-typescript",createdAt:a},{title:"Ktor",tags:["Kotlin"],path:"\u002Fde\u002Fblog\u002Fktor",createdAt:a},{title:"Hello World",path:"\u002Fde\u002Fblog\u002Fhello-world",createdAt:a}]}],fetch:{},mutations:[]}}("2021-03-21T10:35:09.888Z")));
