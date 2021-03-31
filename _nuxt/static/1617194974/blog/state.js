900af81ac9f4f26cea785085399cc34ce5f1c3fc
Jan Müller <janmueller3698@gmail.com>
2021-03-31 12:50:27 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@eb0bb78ba29026a8d5975a1835e46f7205dc7e7d 🚀

---
+++ _nuxt/static/1617194974/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617194974",layout:"default",error:a,state:{title:"home.title",useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fblog",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00b5d595"}},globalRefs:{}}}(null,true));
---
+++ _nuxt/static/1617194974/de/blog/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/de/blog", {data:[{posts:[{title:"Nuxt, Vue and TypeScript",path:"\u002Fde\u002Fblog\u002Fnuxt-vue-and-typescript",createdAt:"2021-03-31T12:48:33.551Z"}]}],fetch:{},mutations:[]});
