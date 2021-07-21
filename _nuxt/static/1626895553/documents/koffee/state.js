fe2ca32fb7c24759f7acb48cc4662d92db4cd2db
Jan Müller <janmueller3698@gmail.com>
2021-07-21 19:26:48 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@dbb9651613a697cf69618d2818d48b3c29e695d4 🚀

---
+++ _nuxt/static/1626895553/documents/koffee/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626895553",layout:"base",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fdocuments\u002Fkoffee",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a56822a"}},globalRefs:{}}}(null,true));
