7469920058a46866aa8b7c87751e8c660454b1c2
Jan Müller <janmueller3698@gmail.com>
2021-07-26 08:59:16 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@ed3c2a15bfd93bd9839b360fcb97596f3d06d73d 🚀

--- _nuxt/static/1627288498/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627288498",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3a35707"}},globalRefs:{}}}(null,true,"\u002F"));
---
+++ _nuxt/static/1627289830/documents/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627289830",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fdocuments",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"60b4d154"}},globalRefs:{}}}(null,true));
