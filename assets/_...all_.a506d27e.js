a6ac6c24589486443c1ab7068e63dedaf8e13e53
Jan Müller <janmueller3698@gmail.com>
2022-07-08 14:49:23 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@d5ac4f5cbd04ad7ce2a84b739cd60e6ff2cee4a0 🚀

---
+++ assets/_...all_.a506d27e.js
@@ -0,0 +1,1 @@
+import{d as e,i as n,o as s,c as a,t as c,u as r}from"./app.c0d92150.js";import{b as t}from"./route-block.b5bad31b.js";const i={class:"text-xl text-light"},l=e({__name:"[...all]",setup(f){const{t:o}=n();return(p,_)=>(s(),a("div",i,c(r(o)("not-found")),1))}});typeof t=="function"&&t(l);export{l as default};
