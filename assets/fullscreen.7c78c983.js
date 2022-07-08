8997866a19610a53a1002453aa5700f09e71c49e
Jan Müller <janmueller3698@gmail.com>
2022-07-08 20:19:33 +0000

    Deploying to gh-pages from @ DerYeger/jan-mueller@f5b2e0d8fd9b7318429018a906897b687df2ac45 🚀

---
+++ assets/fullscreen.7c78c983.js
@@ -0,0 +1,1 @@
+import{h as _,r as s,o as a,c as r,b as e,w as l,f,_ as i,g as m}from"./app.2de0f065.js";const p={},u={class:"w-full h-full flex flex-col"};function d(x,h){const o=i,n=s("router-view"),t=f,c=m;return a(),r("div",u,[e(o),e(t,{class:"flex-1",style:{width:"calc(100% - 2rem)"}},{default:l(()=>[e(n)]),_:1}),e(c)])}var v=_(p,[["render",d]]);export{v as default};
